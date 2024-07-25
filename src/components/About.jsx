bf59f982ec51f17150fc42d645a20fbe82b996fd
manasviwalia2812 <manasviwalia@gmail.com>
2024-07-25 23:56:12 +0530

    added tech stack

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@
 import aboutImg from "../assets/about.jpg";
 import {ABOUT_TEXT} from "../constants";
 const About = () => {
-  return <div className="border-b border-neutral-900 pb-4">
+  return (<div className="border-b border-neutral-900 pb-4">
     <h1 className="text-4xl my-20 text-center">About <span className="text-neutral-500">Me</span></h1>
     <div className="flex flex-wrap">
       <div className="w-full lg:w-1/2 lg:p-8">
@@ -16,7 +16,7 @@ const About = () => {
         </div>
       </div>
     </div>
-  </div>
-}
+  </div>);
+};
 
 export default About
